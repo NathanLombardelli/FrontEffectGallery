37851a90c36257388b6c993e426035caf18b37b7
NathanLombardelli <nathanlombardelli@hotmail.com>
2023-08-19 20:33:37 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
+      build: {
+            rollupOptions: {
+                  input: {
+                        main: resolve(__dirname, 'index.html'),
+                        nested: resolve(__dirname, 'nested/index.html'),
+                  },
+            },
+      },
       base: '/FrontEffectGallery/'
 })
