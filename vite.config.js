bb514900911464b4274c885257ed1a073d2f16ac
NathanLombardelli <nathanlombardelli@hotmail.com>
2023-08-19 20:26:18 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite'
-import atropos from "atropos";
-import swiper from "swiper";
 
 export default defineConfig({
-      plugins:[atropos,swiper],
       base: '/FrontEffectGallery/'
 })
